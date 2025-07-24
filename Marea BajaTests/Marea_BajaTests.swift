3a6f6aa6c4a95325a385a17fcc505c03028c6f96
Tu Nombre <tu.email@ejemplo.com>
2025-07-23 22:28:12 -0600

    Initial Commit

---
+++ Marea BajaTests/Marea_BajaTests.swift
@@ -0,0 +1,16 @@
+//
+//  Marea_BajaTests.swift
+//  Marea BajaTests
+//
+//  Created by Tono Murrieta  on 23/07/25.
+//
+
+import Testing
+
+struct Marea_BajaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
